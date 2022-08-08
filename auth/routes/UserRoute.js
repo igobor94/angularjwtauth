841dc6bb54d0e64fc360f92313cb86160b65ef58
Igor <igor.borowski@capgemini.com>
2022-08-08 12:32:20 +0200

    add authMiddleware to route

--- auth/routes/UserRoute.js
+++ auth/routes/UserRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const user = require('../controllers/UserController')
 const router = express.Router();
 
+const { authMiddleware } = require('../controllers/UserController')
+
 router.post('/register', user.register)
 router.post('/login', user.login)
 
-router.get('/index', function (req, res) {
+router.get('/profile', authMiddleware, function (req, res) {
     res.json({ 'access': true })
 })
 
