5e45e0cec3970cddb1605f79bbca0caa162cd2f9
Igor <igor.borowski@capgemini.com>
2022-08-08 12:40:17 +0200

    add auth routing

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthComponent } from './auth.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
-
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
@@ -11,7 +24,10 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
-  ]
+  ],
+  exports: [RouterModule],
+  providers: [AuthService, AuthGuard]
 })
 export class AuthModule { }
