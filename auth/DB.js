1d9f407ec9804dac4ff46363b62805d22c5ef8ba
Igor <igor.borowski@capgemini.com>
2022-08-08 12:13:16 +0200

    add info about DB.js, add secret example

--- auth/DB.js
+++ auth/DB.js
@@ -1,3 +1,7 @@
+
+// for production bust changes to .env file!!!!!!!
+
 module.exports = {
-    DB: 'mongodb://localhost:27017/angularauth'
+    DB: 'mongodb://localhost:27017/angularauth',
+    secret: 'lool'
   }
