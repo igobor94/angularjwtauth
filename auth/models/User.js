b3a9357592309e94809ea77f0b89558e4eca24e3
Igor <igor.borowski@capgemini.com>
2022-08-08 11:04:24 +0200

    finish userSchema for user

--- auth/models/User.js
+++ auth/models/User.js
@@ -2,9 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        min: [4, 'Too short, min 4 characters are required'],
-        min: [32, 'Too long, max 32 characters are required'],
-    }
+  username: {
+    type: String,
+    min: [4, 'Too short, min 4 characters are required'],
+    min: [32, 'Too long, max 32 characters are required'],
+  },
+  email: {
+    type: String,
+    min: [4, 'Too short, min 4 characters are required'],
+    max: [32, 'Too long, max 16 characters are required'],
+    lowercase: true,
+    unique: true,
+    required: 'Email is required',
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+  },
+  password: {
+    type: String,
+    min: [4, 'Too short, min 4 characters are required'],
+    max: [32, 'Too long, max 16 characters are required'],
+    required: 'password is required'
+  },
+  passwordConfirmation: {
+    type: String,
+    min: [4, 'Too short, min 4 characters are required'],
+    max: [32, 'Too long, max 16 characters are required']
+  }
 })
+
+module.exports = mongoose.model('User', userSchema)
