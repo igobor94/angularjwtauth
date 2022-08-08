f98e2a9705a06985ba4cd7bc2b6910920f360bdf
Igor <igor.borowski@capgemini.com>
2022-08-08 14:16:36 +0200

    init guard & add to routes

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,58 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  private url: string;
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  private authState(): boolean {
+    if (this.isLoginOrRegister()) {
+      this.router.navigate(['/']);
+      return false;
+    }
     return true;
   }
-  
+  private notAuthState(): boolean {
+    if (this.isLoginOrRegister()) {
+      return true;
+    }
+    this.router.navigate(['/auth/login']);
+    return false;
+  }
+  private isLoginOrRegister(): boolean {
+    if (
+      this.url.includes('/auth/login') ||
+      this.url.includes('/auth/register')
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    this.url = state.url;
+    if (this.auth.isAuthenticated()) {
+      return this.authState();
+    }
+    return this.notAuthState();
+  }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,26 +6,32 @@ import { AuthComponent } from './auth.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 const routes: Routes = [
   {
     path: 'auth',
     component: AuthComponent,
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent }
+      { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+      { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] }
     ]
   }
 ];
 
 @NgModule({
   declarations: [
+    AuthComponent,
     RegisterComponent,
     LoginComponent
   ],
   imports: [
     RouterModule.forChild(routes),
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports: [RouterModule],
   providers: [AuthService, AuthGuard]
